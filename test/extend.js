5d13ea199cfe7544d5ae35a08d2f018d677c12b1
Stephen Mathieson <smath23@gmail.com>
2012-12-03 20:42:16 -0500

    test extend

---
+++ test/extend.js
@@ -0,0 +1,50 @@
+/*jslint node:true, es5:true*/
+
+'use strict';
+
+var vows = require('vows');
+
+var assert = require('assert');
+
+var extend = require('../lib/extend.js');
+
+var suite = vows.describe('extend');
+
+suite.addBatch({
+	'two objects': {
+		topic: function () {
+			var a = {
+					'foo': 'bar',
+					'1': 2,
+					'false': true,
+					'cats': 'cats'
+				},
+				b = {
+					'foo': 'bar',
+					'1': 2,
+					'2': 2,
+					'false': false
+				};
+
+			this.callback(null, extend(a, b));
+		},
+
+		'should return an object': function (err, result) {
+			assert.isObject(result);
+		},
+
+		'should favor the second': function (err, result) {
+			assert.equal(result.false, false);
+		},
+
+		'should keep custom first properties': function (err, result) {
+			assert.equal(result.cats, 'cats');
+		},
+
+		'should keep custom second properties': function (err, result) {
+			assert.equal(result['2'], 2);
+		}
+	}
+});
+
+suite.export(module);
