e1a0bc57f7b3269f01c89a526160c8cc8d553e7a
Nathan Smith <nathan.a.smith@live.com>
2014-05-26 22:31:29 +1000

    More CLI tests

--- test/cli.js
+++ test/cli.js
@@ -13,17 +13,38 @@ var execFile = require('child_process').execFile;
 var suite = vows.describe('cli');
 
 suite.addBatch({
-  'sanity': {
+  'defaults': {
     topic: function () {
       execFile(path.join(__dirname, '..', 'bin', 'cpplint'), [
         path.join(__dirname, 'fixtures', 'node-cpp-hello.cpp')
       ], null, this.callback);
     },
 
-    'should run successfully': function (error, stdout, stderr) {
-      assert.isNull(error);
+    'should run successfully': function (err, stdout, stderr) {
+      assert.isNull(err);
       assert.isString(stdout);
       assert.isEmpty(stderr);
+    },
+
+    'default verbosity is 1': function (err, stdout, stderr) {
+      var lines = stdout.split('\n');
+      assert.lengthOf(lines, 7);
+    },
+
+    'default reporter is spec': function (err, stdout, stderr) {
+      var lines = stdout.split('\n');
+      assert.match(lines[0], /✗/);
+      assert.match(lines[0], /node\-cpp\-hello\.cpp/);
+    },
+
+    'default filters is no filters': function (err, stdout, stderr) {
+      var lines = stdout.split('\n');
+      assert.lengthOf(lines, 7);
+    },
+
+    'default extensions is no extensions': function (err, stdout, stderr) {
+      var lines = stdout.split('\n');
+      assert.lengthOf(lines, 7);
     }
   }
 });
